100b64239a977a460b2b94eb2a3edf9a968ac3bd
D4rt-Dy14n <yuri.fedorov@inbox.ru>
2020-05-05 23:31:07 +0300

    Initial Commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Юрий Федоров on 21.04.2020.
+//  Copyright © 2020 Yuri Fedorov. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
